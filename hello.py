8363a903b1ea20c5b73c60493030960948d9051d
Slechtvalkkk <georgviruz@gmail.com>
2016-03-09 20:41:58 +0300

    1.9 step 11

--- hello.py
+++ hello.py
@@ -4,6 +4,6 @@ def app ( environ, start_response ):
         ( 'Content-Type', 'text/plain' )
     ]
     body = environ['QUERY_STRING'].split("&")
-    body = [item+"\r\n" for item in resp]
+    body = [item+"\r\n" for item in body]
     start_response ( status, headers )
     return body
