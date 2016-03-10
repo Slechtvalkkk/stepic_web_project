5e1980f63c5eb67b42ff321978b072f55eb6814d
Slechtvalkkk <georgviruz@gmail.com>
2016-03-10 18:43:05 +0300

    windows

---
+++ hello.py
@@ -0,0 +1,10 @@
+ def app ( environ, start_response ):
+     status = '200 OK'
+     headers = [
+          ( 'Content-Type', 'text/plain' )
+      ]
+      body = environ['QUERY_STRING'].split("&")
+      body = [item+"\r\n" for item in resp]
+      body = [item+"\r\n" for item in body]
+      start_response ( status, headers )
+      return body
