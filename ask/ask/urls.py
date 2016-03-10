b51c8a9de70d24412f3841372c8da8e450e923cb
Slechtvalkkk <georgviruz@gmail.com>
2016-03-10 22:24:18 +0300

    windows

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -4,12 +4,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    #url(r'^', include("qa.urls"))
-    url(r'^$', views.test),
-    url(r'^login/.*$', views.test),
-    url(r'^signup/.*$', views.test),
-    url(r'^questions/\d+/$', views.test),
-    url(r'^ask/.*$', views.test),
-    url(r'^popular/.*$', views.test),
-    url(r'^new/.*$', views.test)
+    url(r'^$', 'qa.views.test'),
+    url(r'^login/.*$', 'qa.views.test'),
+    url(r'^signup/.*$', 'qa.views.test'),
+    url(r'^questions/\d+/$', 'qa.views.test'),
+    url(r'^ask/.*$', 'qa.views.test'),
+    url(r'^popular/.*$', 'qa.views.test'),
+    url(r'^new/.*$', 'qa.views.test')
 ]
