f67d59e0784dde42ef1d86ec3831fe6a79435460
Slechtvalkkk <georgviruz@gmail.com>
2016-03-11 19:12:37 +0300

    windows

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-cd /home/box/web/ask/
-sudo gunicorn --bind 0.0.0.0:8000 --access-logfile log.log ask.wsgi:application
+#cd /home/box/web/ask/
+#sudo gunicorn --bind 0.0.0.0:8000 --access-logfile log.log ask.wsgi:application
